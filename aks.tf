23ceac85615070d2474f4da5656bb32de8a63f26
Taylor Dolezal <onlydole@gmail.com>
2021-02-26 12:55:22 -0800

    Add K8s objects

--- aks.tf
+++ aks.tf
@@ -1,15 +1,19 @@
 # see https://registry.terraform.io/modules/Azure/aks/azurerm/4.7.0
 module "aks" {
   source  = "Azure/aks/azurerm"
-  version = "4.7.0"
+  version = "4.8.0"
 
   kubernetes_version   = var.cluster_version
   orchestrator_version = var.cluster_version
   resource_group_name  = azurerm_resource_group.project.name
   prefix               = var.project_name
+  agents_size          = var.agents_size
+  agents_min_count     = 1
   agents_count         = 3
+  agents_max_count     = 5
   vnet_subnet_id       = module.network.vnet_subnets[0]
   os_disk_size_gb      = 100
+  enable_auto_scaling  = true
 
   # see https://www.terraform.io/docs/language/meta-arguments/depends_on.html
   depends_on = [
